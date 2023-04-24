93369bef5a38680549bab31ad3d137826fd0dbd5
Houman Hafez Alghoran <houman.hafezalghoran@gmail.com>
2023-04-24 12:39:56 +0200

    Update digitalClock.py

--- digitalClock.py
+++ digitalClock.py
@@ -14,9 +14,9 @@ import os
 app_window = Tk() 
 app_window.title("Digital Clock") 
 app_window.geometry("420x150") 
-app_window.resizable(False, False)
+app_window.resizable(0,0)
 
-#Makes a universal path for the favicon and then applies it to the application
+#Makes a universal path for the favicon and then applies it to the app
 script_dir = os.path.dirname(os.path.abspath(__file__))
 icon_path = os.path.join(script_dir, 'icon\icon.ico')
 app_window.iconbitmap(icon_path)
@@ -25,7 +25,7 @@ app_window.iconbitmap(icon_path)
 #Font, Size and Boldiness is defined here
 text_font= ("Boulder", 68, 'bold')
 #Changes the background to a red color
-background = "#f6546a"
+background = "#420420"
 #Changes the Text Color to a Light Blue and gives the border a width
 foreground= "#cccccc"
 border_width = 25
@@ -33,7 +33,7 @@ border_width = 25
 #The whole package above gets added to a Label and then gets a grid layout
 label = Label(app_window, font=text_font, bg=background, fg=foreground, bd=border_width) 
 label.grid(row=0, column=1)
-label.pack()
+
 
 #A function for the Time in Hours, Minutes and Seconds
 def digital_clock(): 
